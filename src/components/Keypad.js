ac48a2462334bc07321e502b7a20b6b082094cdc
Tev-J <tj_delph@yahoo.com>
2023-05-30 04:33:14 -0400

    complete lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  return (
+    <div>
+      <input
+        onChange={(e) => console.log("Entering password...")}
+        type="password"
+      />
+    </div>
+  );
 }
 
 export default Keypad;
